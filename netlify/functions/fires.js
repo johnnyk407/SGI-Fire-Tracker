f0f91c63ed970f4c194cd4a0251386e53c4ccb17
johnnyk407 <johnnyk407@gmail.com>
2026-06-18 21:37:26 -0400

    Update fires.js

--- netlify/functions/fires.js
+++ netlify/functions/fires.js
@@ -1,5 +1,10 @@
 exports.handler = async function() {
-  const url = "https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/WFIGS_Incident_Locations_Current/FeatureServer/0/query?where=IncidentTypeCategory%3D'WF'+AND+PercentContained%3C100&outFields=*&f=geojson";
+  // Active wildfires: WF type, with an ICS-209 status report filed in the last 7 days
+  const sevenDaysAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+  const dateStr = sevenDaysAgo.toISOString().slice(0, 19).replace('T', ' ');
+
+  const where = `IncidentTypeCategory='WF' AND ICS209ReportDateTime>timestamp '${dateStr}'`;
+  const url = 'https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/WFIGS_Incident_Locations_Current/FeatureServer/0/query?where=' + encodeURIComponent(where) + '&outFields=*&f=geojson';
 
   try {
     const response = await fetch(url);
